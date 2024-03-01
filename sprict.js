9b7ddf9b62da855840d4603a1a1c25494746150c
Joao ramos <111580176+joaoramos17092007@users.noreply.github.com>
2024-03-01 11:08:00 -0300

    asd

--- sprict.js
+++ sprict.js
@@ -1,1 +1,12 @@
-console.log("oooooooooooooooooh")
+const botoes = document.querySelectorAll(".btn");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("active");
+        }
+
+        botoes[i].classList.add("active");
+    }
+}
